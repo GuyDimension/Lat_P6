647cbc4af3179aff5dfaee32f1a374a51f4e3566
GuyDimension <billycelino67@gmail.com>
2025-04-22 14:23:43 +0700

    Terakhir

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Matkul from './components/Matkul';
+import ListMatkul from './components/ListMatkul';
 const nama = "Desyana";
 const role = "superadmin";
 
@@ -8,11 +8,7 @@ function App() {
       <h1>Hello, {nama}!</h1>
       <p>I&apos;m boarding now</p>
       <p>Peran: {role}</p>
-
-      <ul>
-        <Matkul kode="BD1007" nama="Pemrograman Web Dasar" />
-        <Matkul kode="BD1019" nama="Pemrograman Web Lanjutan" />
-      </ul>
+      <ListMatkul />
     </div>
   );
 }
